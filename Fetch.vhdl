c8ed6e6f02ac62623eb5aa9b83077fb6795bbf0b
Ivan Martinez <ivan.martinez@bsc.es>
2017-01-20 14:39:46 +0100

    Fetch STAGE working for PC+4, Jump not tested

---
+++ Fetch.vhdl
@@ -0,0 +1,59 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+USE ieee.std_logic_unsigned.all;
+use std.textio.all;
+
+entity Fetch is
+    port (clk : in std_logic;
+          muxIF : in std_logic;
+          boot : in std_logic;
+          PCPlus4F : in std_logic_vector(31 downto 0);
+          PCLocation : in std_logic_vector(31 downto 0);
+          inst, PC : out std_logic_vector(31 downto 0));
+    end Fetch;
+    
+
+    architecture behaviour of Fetch is
+        signal PC_in: std_logic_vector(31 downto 0);
+        signal PCF: std_logic_vector(31 downto 0);
+
+        signal inst_data: std_logic_vector(31 downto 0);
+        signal inst_nwe: std_logic;
+        signal inst_noe: std_logic;
+        signal inst_ncs: std_logic;
+
+        Component Reg is
+            Generic(W : integer);
+            Port (d   : in std_logic_vector(W-1 downto 0);
+            clk : in std_logic;
+            q   : out std_logic_vector(W-1 downto 0));
+        End Component;
+
+        --for PCregister : Reg use entity work.Reg;
+        
+    begin
+            -- PC
+
+                PCregister : Reg generic map(32) port map(PC_in,clk,PCF);
+
+                PC <= PCF+X"00000004";
+
+                PC_in <= PCPlus4F when (muxIF='1' and boot='0') else 
+                         X"00001000" when (boot='1') else
+                        PCLocation;
+
+                --inst <= inst_data;
+    
+            -- Instruction Memory
+               -- inst_memory: entity work.sram64kx8(sram_behaviour)
+                    --port map (inst_ncs, PCF, inst_data, inst_nwe, inst_noe);
+                
+                    -- never write to instruction memory
+                    inst_nwe <= '1';
+                    inst_noe <= '0';
+                    inst_ncs <= '1' when PCF(0)='U' else '0';
+                    
+                    inst_data <= X"11111111";
+                    inst<=inst_data;
+end behaviour;
---
+++ Fetch_Test.vhd
@@ -0,0 +1,62 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use std.textio.all;
+
+entity Fetch_Test is
+end Fetch_Test;
+
+architecture behaviour of Fetch_Test is
+	
+	component Fetch
+	port (clk : in std_logic;
+          muxIF : in std_logic;
+          boot : in std_logic;
+          PCPlus4F : in std_logic_vector(31 downto 0);
+          PCLocation : in std_logic_vector(31 downto 0);
+          inst, PC : out std_logic_vector(31 downto 0));
+	end component;
+
+	for Fetch_0: Fetch use entity work.Fetch;
+		signal clk_in :  std_logic;
+		signal muxIF_in : std_logic;
+        signal boot_in :  std_logic;
+        signal PCLocation_in, inst_in : std_logic_vector(31 downto 0);
+        signal PC_out :  std_logic_vector(31 downto 0);
+
+    begin
+    	Fetch_0: Fetch port map(clk => clk_in,
+    							muxIF => muxIF_in,
+                                boot => boot_in,
+    							PCPlus4F => PC_out,
+    							PCLocation => PCLocation_in,
+                                inst => inst_in,
+    							PC => PC_out
+                                );
+
+    clock: process begin
+        loop
+            clk_in <= '0'; 
+            wait for 1 ns;
+            clk_in <= '1';
+			wait for 1 ns;
+		end loop;
+	end process;
+
+
+    muxIF_in <= '1';
+
+  --PCPlus4F_in<=PC_out;
+
+  boot_process: process begin
+        loop
+            wait for 2 ns;
+            boot_in <= '1';
+            --PCPlus4F_in <= X"00001000";
+            --PC_out <= X"00001000";
+            wait for 2 ns;
+            boot_in<='0';
+            wait for 10 ns;
+        end loop;
+  end process;
+
+  end behaviour;
