e0e88536b15d1e0575300e0e73bce07cb3bfe11d
Ivan Martinez <ivan.martinez@bsc.es>
2017-01-23 22:47:42 +0100

    ·

--- DataCache.vhd
+++ DataCache.vhd
@@ -9,7 +9,8 @@ entity DataCache is
 	port(ADDR: in  std_logic_vector(31 downto 0);
 		 RW : in std_logic; --- 0=READ / 1=WRITE
 		 RW_CONTROL: in std_logic; --- 0=Read permision / 1 = write permision
-		 BYTE_OR_WORD: in std_logic;
+		 BYTE: in std_logic;
+		 WORD: in std_logic;
 		 CPU_OR_MEM: in std_logic; --- 0=MEMORY / 1=CPU
 		 DATA_IN_FROM_MEMORY: in std_logic_vector(127 downto 0);
 		 DATA_IN_FROM_DATAPATH: in std_logic_vector(31 downto 0);
@@ -40,11 +41,37 @@ architecture behav of DataCache is
 	signal writing_from_memory: std_logic;
 	signal writing_from_cpu: std_logic;
 	signal reading_from_memory: std_logic;
-	signal reading_from_cpu:std_logic;
+	signal reading_from_cache:std_logic;
 
 	signal word_out: std_logic_vector(31 downto 0);
 	signal byte_out: std_logic_vector(31 downto 0);
 
+	signal input_byte: std_logic_vector(7 downto 0);
+
+	signal byte_0_0: std_logic_vector(7 downto 0);
+	signal byte_0_1: std_logic_vector(7 downto 0);
+	signal byte_0_2: std_logic_vector(7 downto 0);
+	signal byte_0_3: std_logic_vector(7 downto 0);
+
+	signal byte_1_0: std_logic_vector(7 downto 0);
+	signal byte_1_1: std_logic_vector(7 downto 0);
+	signal byte_1_2: std_logic_vector(7 downto 0);
+	signal byte_1_3: std_logic_vector(7 downto 0);
+
+	signal byte_2_0: std_logic_vector(7 downto 0);
+	signal byte_2_1: std_logic_vector(7 downto 0);
+	signal byte_2_2: std_logic_vector(7 downto 0);
+	signal byte_2_3: std_logic_vector(7 downto 0);
+
+	signal byte_3_0: std_logic_vector(7 downto 0);
+	signal byte_3_1: std_logic_vector(7 downto 0);
+	signal byte_3_2: std_logic_vector(7 downto 0);
+	signal byte_3_3: std_logic_vector(7 downto 0);
+
+	signal word_0: std_logic_vector(31 downto 0);
+	signal word_1: std_logic_vector(31 downto 0);
+	signal word_2: std_logic_vector(31 downto 0);
+	signal word_3: std_logic_vector(31 downto 0);
 
 
 	begin
@@ -56,16 +83,42 @@ architecture behav of DataCache is
 		writing_from_memory<= RW and RW_CONTROL and not CPU_OR_MEM;
 		writing_from_cpu <= RW and RW_CONTROL and CPU_OR_MEM;
 		reading_from_memory<=not RW and RW_CONTROL and not CPU_OR_MEM;
-		reading_from_cpu<= not RW and RW_CONTROL and CPU_OR_MEM;
+		reading_from_cache<= not RW and RW_CONTROL and CPU_OR_MEM;
 		--ON READ
 		tag_eq <= '1' when to_integer(unsigned(tag_store(to_integer(unsigned(set)))))= to_integer(unsigned(tag)) and RW_CONTROL='1' else '0';
 		hit <= '1' when (tag_eq='1' and valid_store(to_integer(unsigned(set)))='1') else '0';
+		input_byte<=DATA_IN_FROM_DATAPATH(7 downto 0);
+
+		byte_0_0 <= data_store(to_integer(unsigned(set)))(7 downto 0);
+		byte_0_1 <= data_store(to_integer(unsigned(set)))(15 downto 8);
+		byte_0_2 <= data_store(to_integer(unsigned(set)))(23 downto 16);
+		byte_0_3 <= data_store(to_integer(unsigned(set)))(31 downto 24);
+
+		byte_1_0 <= data_store(to_integer(unsigned(set)))(39 downto 32);
+		byte_1_1 <= data_store(to_integer(unsigned(set)))(47 downto 40);
+		byte_1_2 <= data_store(to_integer(unsigned(set)))(55 downto 48);
+		byte_1_3 <= data_store(to_integer(unsigned(set)))(63 downto 56);
+
+		byte_2_0 <= data_store(to_integer(unsigned(set)))(71 downto 64);
+		byte_2_1 <= data_store(to_integer(unsigned(set)))(79 downto 72);
+		byte_2_2 <= data_store(to_integer(unsigned(set)))(87 downto 80);
+		byte_2_3 <= data_store(to_integer(unsigned(set)))(95 downto 88);
+
+		byte_3_0 <= data_store(to_integer(unsigned(set)))(103 downto 96);
+		byte_3_1 <= data_store(to_integer(unsigned(set)))(111 downto 104);
+		byte_3_2 <= data_store(to_integer(unsigned(set)))(119 downto 112);
+		byte_3_3 <= data_store(to_integer(unsigned(set)))(127 downto 120);
+
+		word_0 <= data_store(to_integer(unsigned(set)))(31 downto 0);
+		word_1 <= data_store(to_integer(unsigned(set)))(63 downto 32);
+		word_2 <= data_store(to_integer(unsigned(set)))(95 downto 64);
+		word_3 <= data_store(to_integer(unsigned(set)))(127 downto 96);
 
 
-		word_out <= data_store(to_integer(unsigned(set)))(127 downto 96) when reading_from_cpu='1'  and  block_offset="11" else
-				data_store(to_integer(unsigned(set)))(95 downto 64) when reading_from_cpu='1' and block_offset="10" else
-				data_store(to_integer(unsigned(set)))(63 downto 32) when reading_from_cpu='1' and block_offset="01" else
-				data_store(to_integer(unsigned(set)))(31 downto 0) when reading_from_cpu='1' and block_offset="00" else
+		word_out <= data_store(to_integer(unsigned(set)))(127 downto 96) when reading_from_cache='1'  and  block_offset="11" else
+				data_store(to_integer(unsigned(set)))(95 downto 64) when reading_from_cache='1' and block_offset="10" else
+				data_store(to_integer(unsigned(set)))(63 downto 32) when reading_from_cache='1' and block_offset="01" else
+				data_store(to_integer(unsigned(set)))(31 downto 0) when reading_from_cache='1' and block_offset="00" else
 				"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
 		byte_out <= X"000000" & word_out(31 downto 24) when byte_ofset="11" else
@@ -74,8 +127,8 @@ architecture behav of DataCache is
 					X"000000" & word_out(7 downto 0) when byte_ofset="00" else
 					"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-		data_out_to_path<= word_out when BYTE_OR_WORD='0' else 
-							byte_out when BYTE_OR_WORD='1' else
+		data_out_to_path<= word_out when BYTE='0' and WORD='1' else 
+							byte_out when BYTE='1' and WORD='0' else
 							"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
 		data_out_to_memory<=data_store(to_integer(unsigned(set))) when (tag_eq='0' or valid_store(to_integer(unsigned(set)))='0') and dirty_store(to_integer(unsigned(set)))='1';
@@ -84,11 +137,34 @@ architecture behav of DataCache is
 
 
 		--ON WRITE
-		data_store(to_integer(unsigned(set))) <= DATA_IN_FROM_MEMORY when writing_from_memory='1' and writing_from_cpu='0' else
-												 DATA_IN_FROM_DATAPATH & data_store(to_integer(unsigned(set)))(95 downto 0) when writing_from_cpu='1' and  block_offset="11" else
-												 data_store(to_integer(unsigned(set)))(127 downto 96) & DATA_IN_FROM_DATAPATH & data_store(to_integer(unsigned(set)))(63 downto 0) when writing_from_cpu='1' and  block_offset="10" else
-												 data_store(to_integer(unsigned(set)))(127 downto 64) & DATA_IN_FROM_DATAPATH & data_store(to_integer(unsigned(set)))(31 downto 0) when writing_from_cpu='1' and  block_offset="01" else
-												 data_store(to_integer(unsigned(set)))(127 downto 32) & DATA_IN_FROM_DATAPATH  when writing_from_cpu='1' and  block_offset="00" else
+		data_store(to_integer(unsigned(set))) <= data_store(to_integer(unsigned(set))) when reading_from_cache='1' else 
+
+												 DATA_IN_FROM_MEMORY when writing_from_memory='1' and writing_from_cpu='0' else
+
+												 DATA_IN_FROM_DATAPATH & data_store(to_integer(unsigned(set)))(95 downto 0) when WORD='1' and writing_from_cpu='1' and  block_offset="11" else
+												 	input_byte & data_store(to_integer(unsigned(set)))(119 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="11" and byte_ofset="11" else
+												 	byte_3_3 & input_byte & data_store(to_integer(unsigned(set)))(111 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="11" and byte_ofset="10" else
+												 	byte_3_3 & byte_3_2 & input_byte & data_store(to_integer(unsigned(set)))(103 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="11" and byte_ofset="01" else
+												 	byte_3_3 & byte_3_2 & byte_3_1 & input_byte & data_store(to_integer(unsigned(set)))(95 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="11" and byte_ofset="00" else
+
+												 data_store(to_integer(unsigned(set)))(127 downto 96) & DATA_IN_FROM_DATAPATH & data_store(to_integer(unsigned(set)))(63 downto 0) when WORD='1' and writing_from_cpu='1' and  block_offset="10" else
+												 	word_3 & input_byte & data_store(to_integer(unsigned(set)))(87 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="10" and byte_ofset="11" else
+												 	word_3 & byte_2_3 & input_byte & data_store(to_integer(unsigned(set)))(79 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="10" and byte_ofset="10" else
+												 	word_3 & byte_2_3 & byte_2_2 & input_byte & data_store(to_integer(unsigned(set)))(71 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="10" and byte_ofset="01" else
+												 	word_3 & byte_2_3 & byte_2_2 & byte_2_1 & input_byte & data_store(to_integer(unsigned(set)))(63 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="10" and byte_ofset="00" else
+
+												 data_store(to_integer(unsigned(set)))(127 downto 64) & DATA_IN_FROM_DATAPATH & data_store(to_integer(unsigned(set)))(31 downto 0) when WORD='1' and writing_from_cpu='1' and  block_offset="01" else
+												 	word_3 & word_2 & input_byte & data_store(to_integer(unsigned(set)))(55 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="01" and byte_ofset="11" else
+												 	word_3 & word_2 & byte_1_3 & input_byte & data_store(to_integer(unsigned(set)))(47 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="01" and byte_ofset="10" else
+												 	word_3 & word_2 & byte_1_3 & byte_1_2 & input_byte & data_store(to_integer(unsigned(set)))(39 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="01" and byte_ofset="01" else
+												 	word_3 & word_2 & byte_1_3 & byte_1_2 & byte_1_1 & input_byte & data_store(to_integer(unsigned(set)))(31 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="01" and byte_ofset="00" else
+
+												 data_store(to_integer(unsigned(set)))(127 downto 32) & DATA_IN_FROM_DATAPATH  when  WORD='1' and writing_from_cpu='1'  and  block_offset="00" else
+												 	word_3 & word_2 & word_1 & input_byte & data_store(to_integer(unsigned(set)))(23 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="00" and byte_ofset="11" else
+												 	word_3 & word_2 & word_1 & byte_0_3 & input_byte & data_store(to_integer(unsigned(set)))(15 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="00" and byte_ofset="10" else
+												 	word_3 & word_2 & word_1 & byte_0_3 & byte_0_2 & input_byte & data_store(to_integer(unsigned(set)))(7 downto 0) when BYTE='1' and writing_from_cpu='1' and  block_offset="00" and byte_ofset="01" else
+												 	word_3 & word_2 & word_1 & byte_0_3 & byte_0_2 & byte_0_1 & input_byte when BYTE='1' and writing_from_cpu='1' and  block_offset="00" and byte_ofset="00" else
+
 												 data_store(to_integer(unsigned(set)));			
 			
 		--data_store(to_integer(unsigned(set)))(127 downto 96) <= DATA_IN_FROM_DATAPATH when writing_from_cpu='1' and  block_offset="11" else data_store(to_integer(unsigned(set)));
--- DataCache_Test.vhd
+++ DataCache_Test.vhd
@@ -11,7 +11,8 @@ architecture behaviour of DataCache_Test is
 	port(ADDR: in  std_logic_vector(31 downto 0);
      RW : in std_logic; --- 0=READ / 1=WRITE
      RW_CONTROL: in std_logic; --- 0=Read permision / 1 = write permision
-     BYTE_OR_WORD: in std_logic;
+     BYTE: in std_logic;
+     WORD: in std_logic;
      CPU_OR_MEM: in std_logic; --- 0=MEMORY / 1=CPU
      DATA_IN_FROM_MEMORY: in std_logic_vector(127 downto 0);
      DATA_IN_FROM_DATAPATH: in std_logic_vector(31 downto 0);
@@ -24,7 +25,8 @@ architecture behaviour of DataCache_Test is
 		 signal ADDR_in: std_logic_vector(31 downto 0);
      signal RW_in : std_logic; --- 0=READ / 1=WRITE
      signal RW_CONTROL_in: std_logic; --- 0=Read permision / 1 = write permision
-     signal BYTE_OR_WORD_in: std_logic;
+     signal BYTE_in: std_logic;
+     signal WORD_in: std_logic;
      signal CPU_OR_MEM_in: std_logic; --- 0=MEMORY / 1=CPU
      signal DATA_IN_FROM_MEMORY_in: std_logic_vector(127 downto 0);
      signal DATA_IN_FROM_DATAPATH_in:  std_logic_vector(31 downto 0);
@@ -38,7 +40,8 @@ architecture behaviour of DataCache_Test is
     							            RW => RW_in,
                               RW_CONTROL => RW_CONTROL_in,
                               CPU_OR_MEM => CPU_OR_MEM_in,
-                              BYTE_OR_WORD=>BYTE_OR_WORD_in,
+                              BYTE=>BYTE_in,
+                              WORD => WORD_in,
     							            DATA_IN_FROM_MEMORY => DATA_IN_FROM_MEMORY_in,
                               DATA_IN_FROM_DATAPATH=>DATA_IN_FROM_DATAPATH_in,
     							            data_out_to_memory => data_out_to_memory_in,
@@ -48,127 +51,61 @@ architecture behaviour of DataCache_Test is
 
   test_process: process begin
         loop
-            wait for 4 ns;
-            ADDR_in<=X"00002000";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000003000000020000000100000000";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 4 ns;
-            ADDR_in<=X"00002000";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='0';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000003000000020000000100000000";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
             wait for 2 ns;
-            ADDR_in<=X"00002000";
+            ADDR_in<=X"00002000"; --STB(miss)
             RW_in<='1';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='0';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000003000000020000000100000000";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002000";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000003000000020000000100000000";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002004";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000003000000020000000100000000";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002008";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000003000000020000000100000000";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002010";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000003000000020000000100000000";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002010";
-            RW_in<='0';
             RW_CONTROL_in<='0';
-            CPU_OR_MEM_in<='0';
-            BYTE_OR_WORD_in<='0';
+            CPU_OR_MEM_in<='1';
+            BYTE_in<='1';
+            WORD_in<='0';
             DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
+            DATA_IN_FROM_DATAPATH_in<=X"000000FF";
             wait for 2 ns;
-            ADDR_in<=X"00002010";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='0';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002010";
+            ADDR_in<=X"00002000"; --memory respond with the line
             RW_in<='1';
             RW_CONTROL_in<='1';
             CPU_OR_MEM_in<='0';
-            BYTE_OR_WORD_in<='0';
+            BYTE_in<='1';
+            WORD_in<='0';
             DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
+            DATA_IN_FROM_DATAPATH_in<=X"000000FF";
             wait for 2 ns;
-            ADDR_in<=X"00002010";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002014";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002014";
-            RW_in<='0';
-            RW_CONTROL_in<='1';
-            CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
-            DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-            wait for 2 ns;
-            ADDR_in<=X"00002000";
+            ADDR_in<=X"00002000"; --STB(hit)
             RW_in<='1';
             RW_CONTROL_in<='1';
             CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
+            BYTE_in<='1';
+            WORD_in<='0';
             DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
+            DATA_IN_FROM_DATAPATH_in<=X"000000FF";
             wait for 2 ns;
-            ADDR_in<=X"00002000";
+            ADDR_in<=X"00002002"; --STB(hit)
+            RW_in<='1';
+            RW_CONTROL_in<='1';
+            CPU_OR_MEM_in<='1';
+            BYTE_in<='1';
+            WORD_in<='0';
+            DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
+            DATA_IN_FROM_DATAPATH_in<=X"000000FF";
+            wait for 2 ns;
+            ADDR_in<=X"00002003"; --STB(hit)
+            RW_in<='1';
+            RW_CONTROL_in<='1';
+            CPU_OR_MEM_in<='1';
+            BYTE_in<='1';
+            WORD_in<='0';
+            DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
+            DATA_IN_FROM_DATAPATH_in<=X"000000FF";
+            wait for 2 ns;
+            ADDR_in<=X"00002000";--lw(hit)
+            BYTE_in<='0';
+            WORD_in<='1';
             RW_in<='0';
             RW_CONTROL_in<='1';
             CPU_OR_MEM_in<='1';
-            BYTE_OR_WORD_in<='0';
             DATA_IN_FROM_MEMORY_in<=X"00000007000000060000000500000004";
-            DATA_IN_FROM_DATAPATH_in<=X"88888888";
-
+            DATA_IN_FROM_DATAPATH_in<=X"66666666";
+            wait for 2 ns;
         end loop;
   end process;
 
